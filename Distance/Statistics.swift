26aa3707c23c837b722f66b45679e7d4b0aa42e8
David Maitland <hello@davidmaitland.me>
2018-06-07 23:53:40 +0100

    Just about working 🚲

---
+++ Distance/Statistics.swift
@@ -0,0 +1,101 @@
+//
+//  Statistics.swift
+//  Distance
+//
+//  Created by David Maitland on 07/06/2018.
+//  Copyright © 2018 David Maitland. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+var healthStore: HKHealthStore?
+
+func requestAuthorization() -> Bool {
+    
+    // Request access to workoutType, distanceCycling
+    
+    let allTypes = Set([HKObjectType.workoutType(),
+                        HKObjectType.quantityType(forIdentifier: .distanceCycling)!])
+    
+    healthStore!.requestAuthorization(toShare: nil, read: allTypes) { (success, error) in
+        if !success {
+            print("requestAuthorization failed")
+        } else {
+            print("requestAuthorization success")
+        }
+    }
+    
+    // ToDo: Return success, error
+    
+    return true
+
+}
+
+func setupHealthStore() {
+    
+    // Initialise HKHealthStore - is there a better way to do this with the optional?
+    
+    if (healthStore == nil) {
+        healthStore = HKHealthStore()
+        print("Assigned a HKHealthStore")
+    } else {
+        print("We already have a HKHealthStore")
+    }
+    
+    let _ = requestAuthorization()
+    
+}
+
+func getData(completionHandler:@escaping (Double?)->()) {
+    
+    if HKHealthStore.isHealthDataAvailable() {
+        
+        setupHealthStore()
+    
+        // Setup date range
+        
+        // ToDo: Get range from params
+    
+        let calendar = Calendar.current
+        let now = Date.init()
+        let start = calendar.date(byAdding: .day, value: -200, to: Date())
+        
+        let sampleType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceCycling)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: start, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: sampleType!,
+                                      quantitySamplePredicate: predicate,
+                                      options: .cumulativeSum) { query, result, error in
+                                        
+                                        if result != nil {
+                                            
+                                            var total = 0.0
+                                            
+                                            if let quantity = result?.sumQuantity() {
+                                                
+                                                // ToDo: Get unit from params
+                                                
+                                                let unit = HKUnit.mile()
+                                                total = quantity.doubleValue(for: unit)
+                                                
+                                            }
+                                            
+                                            // ToDo: Learn about Grand Central Dispatch
+                                            
+                                            DispatchQueue.main.async {
+                                                completionHandler(total)
+                                            }
+                                    
+                                        }
+                                        
+                                    }
+        
+        healthStore!.execute(query)
+        
+    } else {
+        // ToDo: Display error to user and quit
+    }
+    
+}
--- Distance/ViewController.swift
+++ Distance/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var UILabelTotal: UILabel!
+    
+    @IBAction func ButtonRefresh(_ sender: Any) {
+        self.UILabelTotal.text = "0"
+        getData(completionHandler: updateView)
+    }
+    
+    func updateView(total: Double?) {
+        
+        let rounded = Int(total!.rounded())
+        let display = "\(rounded)"
+        self.UILabelTotal.text = display
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup label
+        
+        self.UILabelTotal.text = "0"
+        self.UILabelTotal.layer.masksToBounds = true
+        self.UILabelTotal.layer.cornerRadius = 5
+        
+        // ToDo: Move this out of the view, do I need to?
+        
+        getData(completionHandler: updateView)
+        
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
     }
 
     override func didReceiveMemoryWarning() {
